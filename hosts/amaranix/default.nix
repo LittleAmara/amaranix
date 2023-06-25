0d2ad770db00ca6eaf3558e646e7a436b46e4a11
Alexandre Guenot <alexandre.guenot@epita.fr>
2023-06-25 21:39:56 +0200

    feat(amaranix): add hyprland cachix

--- hosts/amaranix/default.nix
+++ hosts/amaranix/default.nix
@@ -121,7 +121,11 @@
   # Nix configuration
   nix = {
     package = pkgs.nixVersions.nix_2_15;
-    settings.experimental-features = [ "nix-command" "flakes" ];
+    settings = {
+      substituters = [ "https://hyprland.cachix.org" ];
+      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+      experimental-features = [ "nix-command" "flakes" ];
+    };
     registry.nixpkgs.flake = inputs.nixpkgs;
     nixPath = [ "nixpkgs=/etc/channels/nixpkgs" "nixos-config=/etc/nixos/configuration.nix" "/nix/var/nix/profiles/per-user/root/channels" ];
   };
