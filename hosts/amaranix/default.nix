85b24a6cfae371438b7c48ec557d3e874780421c
Alexandre Guenot <alexandre.guenot@epita.fr>
2023-09-17 11:54:27 +0200

    fix(hosts: amaranix): remove nvidia prime because it fucked up my config

--- hosts/amaranix/default.nix
+++ hosts/amaranix/default.nix
@@ -84,17 +84,6 @@
     nvidiaSettings = true;
   };
 
-  hardware.nvidia.prime = {
-    offload = {
-      enable = true;
-      enableOffloadCmd = true;
-    };
-
-    # Make sure to use the correct Bus ID values for your system!
-    intelBusId = "PCI:0:2:0";
-    nvidiaBusId = "PCI:2:0:0";
-  };
-
   virtualisation.docker.enable = true;
 
   # Configure keymap in X11
