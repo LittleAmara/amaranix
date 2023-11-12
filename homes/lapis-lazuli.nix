3c7ff4f5b9a3b59a10ca673c2065f6c778bf75a5
Alexandre Guenot <alexandre.guenot@epita.fr>
2023-11-12 23:55:11 +0100

    refacto(homes: lapis-lazuli): remove packages config

--- homes/lapis-lazuli.nix
+++ homes/lapis-lazuli.nix
@@ -3,8 +3,10 @@
 rec {
 
   imports = [
-  ../modules/zsh.nix
-  ../modules/tmux.nix
+    ../modules/fzf.nix
+    ../modules/zsh.nix
+    ../modules/tmux.nix
+    ../modules/zoxide.nix
   ];
 
   home = {
@@ -21,40 +23,6 @@ rec {
     ".config/appolox/toto".source = pkgs.emptyFile;
   };
 
-  programs.zoxide = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-  programs.fzf = {
-    enable = true;
-    enableZshIntegration = true;
-    defaultCommand = ''
-      fd --color=always --type f --strip-cwd-prefix --hidden --exclude .git
-    '';
-    defaultOptions = [
-      "--layout=reverse"
-      "--height=75%"
-      "-m"
-      "--ansi"
-      ''--bind="ctrl-space:toggle-preview"''
-      "--preview='bat --color=always {}'"
-      "--preview-window=:hidden"
-    ];
-    colors = {
-      "bg+" = "#414559";
-      bg = "#303446";
-      spinner = "#f2d5cf";
-      hl = "#e78284";
-      fg = "#c6d0f5";
-      header = "#e78284";
-      info = "#ca9ee6";
-      pointer = "#f2d5cf";
-      marker = "#f2d5cf";
-      "fg+" = "#c6d0f5";
-      prompt = "#ca9ee6";
-      "hl+" = "#e78284";
-    };
-  };
 
   home.stateVersion = "23.05";
 
