1d43d466e8bbf4269208354a03d75aa4b6c28c4b
Alexandre Guenot <alexandre.guenot@epita.fr>
2023-11-20 23:16:19 +0100

    feat(modules: bat): init bat module

---
+++ modules/bat.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, lib, ... }:
+
+{
+  programs.bat = {
+    enable = true;
+    config = {
+      theme = "catppuccin-frappe";
+    };
+    themes = {
+      catppuccin-frappe = {
+        src = pkgs.fetchFromGitHub {
+          owner = "catppuccin";
+          repo = "bat";
+          rev = "master";
+          sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+        };
+        file = "Catppuccin-frappe.tmTheme";
+      };
+    };
+  };
+}
