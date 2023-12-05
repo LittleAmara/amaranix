7abfbd52c44f5d04b2321fd61eee384cb322df11
Alexandre Guenot <alexandre.guenot@epita.fr>
2023-12-05 20:26:55 +0100

    feat(modules: themes): add a module that takes care of all the colors

---
+++ modules/themes.nix
@@ -0,0 +1,84 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+let
+  mkColorOption = name: {
+    inherit name;
+    value = mkOption {
+      type = types.strMatching "#[a-fA-F0-9]{6}";
+      description = "Hexadecimal colors";
+      default = "#000000";
+    };
+  };
+
+  colors = [
+    "rosewater"
+    "flamingo"
+    "pink"
+    "mauve"
+    "red"
+    "maroon"
+    "peach"
+    "yellow"
+    "green"
+    "teal"
+    "sky"
+    "sapphire"
+    "blue"
+    "lavender"
+    "text"
+    "subtext1"
+    "subtext0"
+    "overlay2"
+    "overlay1"
+    "overlay0"
+    "surface2"
+    "surface1"
+    "surface0"
+    "base"
+    "mantle"
+    "crust"
+  ];
+
+  mkThemesOption = name: {
+    inherit name;
+    value.colors = listToAttrs (map mkColorOption colors);
+  };
+
+  catppucinThemes = [ "frappe" "mocha" ];
+in
+{
+  options.themes = listToAttrs (map mkThemesOption catppucinThemes);
+
+  config.themes =
+    {
+      mocha.colors = {
+        rosewater = "#f5e0dc";
+        flamingo = "#f2cdcd";
+        pink = "#f5c2e7";
+        mauve = "#cba6f7";
+        red = "#f38ba8";
+        maroon = "#eba0ac";
+        peach = "#fab387";
+        yellow = "#f9e2af";
+        green = "#a6e3a1";
+        teal = "#94e2d5";
+        sky = "#89dceb";
+        sapphire = "#74c7ec";
+        blue = "#89b4fa";
+        lavender = "#b4befe";
+        text = "#cdd6f4";
+        subtext1 = "#bac2de";
+        subtext0 = "#a6adc8";
+        overlay2 = "#9399b2";
+        overlay1 = "#7f849c";
+        overlay0 = "#6c7086";
+        surface2 = "#585b70";
+        surface1 = "#45475a";
+        surface0 = "#313244";
+        base = "#1e1e2e";
+        mantle = "#181825";
+        crust = "#11111b";
+      };
+    };
+}
