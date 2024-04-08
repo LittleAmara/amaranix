cbe12f45e84e8442dd2b02c00bd8c82f423d2c95
Alexandre Guenot <alexandre.guenot@epita.fr>
2024-04-08 17:50:09 +0200

    feat(hosts: lapis-lazuli): update bluetooth settings

--- hosts/lapis-lazuli/configuration.nix
+++ hosts/lapis-lazuli/configuration.nix
@@ -24,6 +24,12 @@
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = false;
+    settings = {
+      General = {
+        Experimental = true;
+        Enable = "Source,Sink,Media,Socket";
+      };
+    };
   };
   services.blueman.enable = true;
 
